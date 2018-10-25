c3cc204fcf60695a63220340bf5bde64ef29de6d
“Marksoloff” <marksoloff@gmail.com>
2018-10-24 21:30:51 -0500

    big theme changes

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-  
-    </div>
+    <header>
+         <section id="sidebar">
+        <div class="inner">
+          <nav>
+            <ul>                                      
+              <li><a href="#Home">Home</a></li>
+              <li><a href="#Swap">Swap</a></li>
+              <li><a href="#Add">Add</a></li>
+              <li><a href="#Redact">Redact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </section>
+    </header>
     <router-view/>
+    <footer id="footer" class="wrapper style1-alt">
+      <div class="inner">
+        <ul class="menu">
+          <li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+        </ul>
+      </div>
+    </footer>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,82 @@
 <template>
-  <div class="articles-index">
-    <div class="row">
-      <div class="col-md-4" v-for="article in articles">
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="card-title">{{ article.headline }}</h5>
-            <div class="card-text">
+  <div id="app">
+      <div id="wrapper">
+        <!-- Intro -->
+          <section id="intro" class="wrapper style1 fullscreen fade-up">
+            <div class="inner">
+              <h1>Fake News</h1>
+              <p>Breaking news broken daily</p>
+              <ul class="actions">
+                <li><a href="#one" class="button scrolly">Get Crackin'</a></li>
+              </ul>
             </div>
-            <router-link class="btn btn-primary" v-bind:to="'/articles/'+ article.id +'/show' ">Read More</router-link>
-          </div>
+          </section>
+
+        <!-- One -->
+          <section id="Article One" class="wrapper style2 spotlights">
+            <section>
+
+              <a href="#" class="image"><img src="images/pic01.jpg" alt="" data-position="center center" /></a>
+              <div class="content">
+                <div class="inner">
+                  <div class="container">
+                       <h1>{{ message }}</h1>
+                       <div>
+                         <h1> {{ article.headline }}</h1>
+                         <h1> {{ article.body }}</h1>
+                      </div>
+                    </div>
+                  <ul class="actions">
+                    <li><a href="generic.html" class="button">Learn more</a></li>
+                  </ul>
+                </div>
+              </div>
+            </section>
+            <section>
+              <a href="#" class="image"><img src="images/pic02.jpg" alt="" data-position="top center" /></a>
+              <div class="content">
+                <div class="inner">
+                  <h2>Feugiat consequat</h2>
+                  <p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
+                  <ul class="actions">
+                    <li><a href="generic.html" class="button">Learn more</a></li>
+                  </ul>
+                </div>
+              </div>
+            </section>
+            <section>
+              <a href="#" class="image"><img src="images/pic03.jpg" alt="" data-position="25% 25%" /></a>
+              <div class="content">
+                <div class="inner">
+                  <h2>Ultricies aliquam</h2>
+                  <p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
+                  <ul class="actions">
+                    <li><a href="generic.html" class="button">Learn more</a></li>
+                  </ul>
+                </div>
+              </div>
+            </section>
+          </section>
+        <!-- Three -->
+          <section id="three" class="wrapper style1 fade-up">
+            <div class="inner">
+              <div class="split style1">
+                <section>   
+                      <h3>Social</h3>
+                      <ul class="icons">
+                        <li><a href="#" class="fa-twitter"><span class="label">Twitter</span></a></li>
+                        <li><a href="#" class="fa-facebook"><span class="label">Facebook</span></a></li>
+                        <li><a href="#" class="fa-github"><span class="label">GitHub</span></a></li>
+                        <li><a href="#" class="fa-instagram"><span class="label">Instagram</span></a></li>
+                        <li><a href="#" class="fa-linkedin"><span class="label">LinkedIn</span></a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </section>
+              </div>
+            </div>
+          </section>
         </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -34,9 +99,18 @@ export default {
       this.articles = response.data;
     }.bind(this));
   },
-  methods: {},
+ 
+  methods: {
+    goToSwap: function() {
+      axios
+      .get("http://localhost:3000/api/articles") + this.$route.params.id + "?version=swap"}
+  },
   computed: {}
-};
+}
+
 </script>
 
+<!-- http://localhost:3000/api/articles/3?version=swap -->
+
+     
 
