c790f58c7311d14c1c1a6a96754520eb06152a4d
“Marksoloff” <marksoloff@gmail.com>
2019-06-29 14:29:18 -0500

    built for heroku

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname));
-var port = process.env.PORT || 5000;
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
+
+// var path = require('path');
+// var serveStatic = require('serve-static');
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html");  
+});
+
 app.listen(port);
-console.log('server started '+ port);
+console.log("server started...");
