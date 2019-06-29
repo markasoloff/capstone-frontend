74f411f91bdf43128a04eee9dccb8ea234832b6e
“Marksoloff” <marksoloff@gmail.com>
2019-06-29 10:32:38 -0500

    routes

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue';
 import router from './router';
 import axios from 'axios';
 
+var history = require('connect-history-api-fallback');
+
+var express = require('express');
+
+var app = express();
+app.use(history());
 
 var SocialSharing = require('vue-social-sharing');
 Vue.use(SocialSharing);
