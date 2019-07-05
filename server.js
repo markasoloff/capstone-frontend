79e5eb09f04cab71005bcf5ae2dfefea1aca75a4
“Marksoloff” <marksoloff@gmail.com>
2019-07-04 21:23:55 -0500

    updated index.html

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res) {
-  res.sendfile(__dirname + "/dist/swap.html");  
+  res.sendfile(__dirname + "/dist/index.html");  
 });
 
 // app.listen(port);
